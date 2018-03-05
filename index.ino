3b388fb495dcb78b1ef91e3e8830859c6754e0c7
Vince van Noort <vincevannoort@gmail.com>
2018-03-05 17:14:07 +0100

    written part of the menu

--- index.ino
+++ index.ino
@@ -1,5 +1,4 @@
-#include <LiquidCrystal.h>
-
+#include <EEPROM.h>
 #include <OneWire.h>
 #include <LiquidCrystal.h>
 #include <DallasTemperature.h>
@@ -27,15 +26,23 @@ enum Pins {
   LIGHT_PIN = 0,
   MOTION_PIN = 8,
   TEMPERATURE_BUS = 12,
-  BUTTON_SPRAY = 4,
-  BUTTON_ANALYSE = 3,
-  BUTTON_MENU = 2
-  LCD_RS = 6,
-  LCD_EN = 5,
-  LCD_D4 = A2,
-  LCD_D5 = A3,
-  LCD_D6 = A4,
-  LCD_D7 = A5
+  BUTTON_PREVIOUS = 4,
+  BUTTON_NEXT = 3,
+  BUTTON_MENU = 2,
+  LCD_RS = 5,
+  LCD_EN = 6,
+  LCD_D4 = A5,
+  LCD_D5 = A4,
+  LCD_D6 = A3,
+  LCD_D7 = A2
+};
+
+enum Menu {
+  START,
+  SPRAYS,
+  DELAY,
+  RESET,
+  END
 };
 
 OneWire one_wire_bus(TEMPERATURE_BUS);
@@ -46,11 +53,24 @@ LiquidCrystal lcd(Pins::LCD_RS, Pins::LCD_EN, Pins::LCD_D4, Pins::LCD_D5, Pins::
 enum Variables {
   MAX_TIME_NUMBER1 = 30000,
   TIME_BEFORE_MEASURING_AFTER_DOOR_CHANGE = 7500,
-  LIGHT_THRESHOLD = 970
+  LIGHT_THRESHOLD = 970,
+  INITIAL_EXTRA_SPRAY_DELAY = 500,
+  INITIAL_NUMBER_OF_SPRAYS = 2400
 };
 
 int previous_state = State::SETUP;
 int current_state = State::SETUP;
+int current_menu_state = Menu::START;
+
+/**
+ * EEPROM ADDRESSES
+ */
+// addresses
+int eea_sprays = 0;
+int eea_delay = 0;
+// data
+int dee_sprays = 0;
+int dee_delay = 0;
 
 int DOOR_STATUS = 0;
 bool DOOR_STATUS_CHANGED = false;
@@ -59,7 +79,6 @@ int CURRENT_ACTION = millis();
 
 /**
  * Sensors  
- * TODO: move into own files (.hpp and .cpp)
  */
 class Sensors {
   public:
@@ -118,8 +137,7 @@ class Sensors {
    * Motion sensor
    */
   static bool is_movement_detected() {
-    return true;
-    // return digitalRead(Pins::MOTION_PIN);
+    return digitalRead(Pins::MOTION_PIN);
   }
   static bool is_no_movement_detected() {
     return !Sensors::is_movement_detected();
@@ -153,24 +171,64 @@ class Sensors {
     temperature_sensor.requestTemperatures();
     return temperature_sensor.getTempCByIndex(0);
   }
-  static void display_temperature() {
+
+  /**
+   * Display
+   */
+  static void display_temperature_and_shots() {
     if (lcd_update_count++ % 2000 == 0) {
+      // temperature
       lcd.setCursor(1, 0);
       lcd.print(" Temp: ");
-      lcd.print(Sensors::get_temperature(), DEC);
+      // lcd.print(Sensors::get_temperature(), DEC);
       lcd.setCursor(12, 0);
       lcd.print((char)223);
       lcd.setCursor(13, 0);
       lcd.print("C   ");
+
+      // shots
+      lcd.setCursor(1, 1);
+      lcd.print(" Shots: ");
+      lcd.print(Sensors::amount_of_air_refreshener_sprays_left);
     }
+  }
+
+  static void display_menu() {
+    lcd.setCursor(1, 0);
+    switch(current_menu_state) {
+      case Menu::START: {
+        lcd.print("Menu: Start");
+        break;
+      }
+      case Menu::SPRAYS: {
+        lcd.print("Menu: Sprays");
+        break;
+      }
+      case Menu::DELAY: {
+        lcd.print("Menu: Delay");
+        break;
+      }
+      case Menu::RESET: {
+        lcd.print("Menu: Reset");
+        break;
+      }
+    }
+  }
+
+  static void next_menu() {
+    current_menu_state = ++current_menu_state % 5;
+    lcd.clear();
+    delay(500);
   }
 
   /**
    * Air refreshener
    */
   static int amount_of_air_refreshener_sprays_left;
+  static int delay_before_spraying;
   static void spray_air_refreshener() {
     Sensors::amount_of_air_refreshener_sprays_left--;
+    EEPROM.put(eea_sprays, Sensors::amount_of_air_refreshener_sprays_left);
     Sensors::set_rgb_led_color(255, 0, 0);
     delay(500);
     Sensors::set_rgb_led_color(255, 255, 255);
@@ -212,7 +270,8 @@ class Sensors {
 
 };
 // setup amount of sprays left
-int Sensors::amount_of_air_refreshener_sprays_left = 2400;
+int Sensors::amount_of_air_refreshener_sprays_left = Variables::INITIAL_NUMBER_OF_SPRAYS;
+int Sensors::delay_before_spraying = Variables::INITIAL_EXTRA_SPRAY_DELAY;
 
 /**
  * setup
@@ -227,11 +286,17 @@ void setup()
   pinMode(Pins::DOOR_PIN, INPUT_PULLUP);
   pinMode(Pins::LIGHT_PIN, INPUT);
   pinMode(Pins::MOTION_PIN, INPUT);
-  pinMode(Pins::BUTTON_SPRAY, INPUT_PULLUP);
-  pinMode(Pins::BUTTON_ANALYSE, INPUT_PULLUP);
+  pinMode(Pins::BUTTON_PREVIOUS, INPUT_PULLUP);
+  pinMode(Pins::BUTTON_NEXT, INPUT_PULLUP);
   pinMode(Pins::BUTTON_MENU, INPUT_PULLUP);
   temperature_sensor.begin();
   lcd.begin(16, 2);
+
+  // get values from EEPROM
+  EEPROM.get(eea_sprays, dee_sprays);
+  EEPROM.get(eea_delay, dee_delay);
+  Sensors::amount_of_air_refreshener_sprays_left = dee_sprays;
+  Sensors::delay_before_spraying = dee_delay;
 
   // switch to begin status
   Sensors::switch_status(State::NOT_IN_USE);
@@ -245,11 +310,13 @@ void loop()
   /**
    * Buttons
    */
-  if (Sensors::is_button_pressed(Pins::BUTTON_SPRAY)) { Sensors::switch_status(State::SPRAYING); }
-  if (Sensors::is_button_pressed(Pins::BUTTON_ANALYSE)) { Sensors::switch_status(State::NOT_IN_USE); }
-  if (Sensors::is_button_pressed(Pins::BUTTON_MENU)) { Sensors::switch_status(State::MENU_ACTIVE); }
-  // Sensors::display_temperature();
-  // Sensors::print_debug_information_to_serial();
+  if (current_state != State::MENU_ACTIVE) {
+    if (Sensors::is_button_pressed(Pins::BUTTON_PREVIOUS)) { Sensors::switch_status(State::SPRAYING); }
+    if (Sensors::is_button_pressed(Pins::BUTTON_NEXT)) { Sensors::switch_status(State::NOT_IN_USE); }
+    if (Sensors::is_button_pressed(Pins::BUTTON_MENU)) { Sensors::switch_status(State::MENU_ACTIVE); }
+    Sensors::display_temperature_and_shots();
+    // Sensors::print_debug_information_to_serial();
+  }
 
   /**
    * States
@@ -381,8 +448,11 @@ void loop()
      */
     case State::SPRAYING:
     {
-      // show that in spraying state
-      delay(500);
+      /**
+       * show that in spraying state
+       * using delay justified since, it shouldnt be able to exit the spraying state without finishing, since that could leave the motor on
+       */
+      delay(Sensors::delay_before_spraying);
 
       /**
        *  Spray once
@@ -415,7 +485,45 @@ void loop()
      */
     case State::MENU_ACTIVE:
     {
-      // functions
+      // display menu
+      Sensors::display_menu();
+
+      // previous
+      // if (Sensors::is_button_pressed(Pins::BUTTON_NEXT)) { Sensors::switch_status(State::NOT_IN_USE); }
+
+      // next
+      // if (Sensors::is_button_pressed(Pins::BUTTON_NEXT)) { Sensors::switch_status(State::NOT_IN_USE); }
+      
+      switch(current_menu_state) {
+        case Menu::START: {
+          break;
+        }
+        case Menu::SPRAYS: {
+          break;
+        }
+        case Menu::DELAY: {
+          break;
+        }
+        case Menu::RESET: {
+          if (Sensors::is_button_pressed(Pins::BUTTON_PREVIOUS) && Sensors::is_button_pressed(Pins::BUTTON_NEXT)) {
+            EEPROM.put(eea_delay, Variables::INITIAL_EXTRA_SPRAY_DELAY);
+            EEPROM.put(eea_sprays, Variables::INITIAL_NUMBER_OF_SPRAYS);
+            Sensors::amount_of_air_refreshener_sprays_left = Variables::INITIAL_NUMBER_OF_SPRAYS;
+            Sensors::delay_before_spraying = Variables::INITIAL_EXTRA_SPRAY_DELAY;
+            Sensors::switch_status(State::NOT_IN_USE);
+            delay(1000);
+          }
+          break;
+        }
+      }
+
+      // back to menu
+      if (Sensors::is_button_pressed(Pins::BUTTON_MENU)) {
+        Sensors::next_menu();
+        if (current_menu_state == Menu::END) {
+          Sensors::switch_status(State::NOT_IN_USE);
+        }
+      }
       break;
     }
 
